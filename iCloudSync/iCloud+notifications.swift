268fa5fa5d913ce00e438f8edf3d06b4d20f861c
oskarirauta <oskari.rauta@gmail.com>
2018-12-26 18:14:58 +0200

    Initial release
    Completely rewritten Swift version.

---
+++ iCloudSync/iCloud+notifications.swift
@@ -0,0 +1,44 @@
+//
+//  iCloud+notifications.swift
+//  iCloud
+//
+//  Created by Oskari Rauta on 25/12/2018.
+//  Copyright © 2018 Oskari Rauta. All rights reserved.
+//
+
+import Foundation
+
+extension iCloud {
+    
+    @objc open func startUpdate(_ notification: Notification) {
+        // Log file update
+        if self.verboseLogging { NSLog("[iCloud] Beginning file update with NSMetadataQuery") }
+        
+        // Notify delegate of the results
+        if self.delegate?.responds(to: #selector(iCloudDelegate.iCloudFileUpdateDidBegin)) ?? false {
+            DispatchQueue.main.async { self.delegate?.iCloudFileUpdateDidBegin?() }
+        }
+        
+    }
+    
+    @objc open func receivedUpdate(_ notification: Notification) {
+        // Log file update
+        if self.verboseLogging { NSLog("[iCloud] An update has been pushed from iCloud with NSMetadataQuery") }
+        
+        // Get the updated files
+        self.updateFiles()
+    }
+    
+    @objc open func endUpdate(_ nofication: Notification) {
+        // Get the updated files
+        self.updateFiles()
+
+        // Notify the delegate of the results on the main thread
+        if self.delegate?.responds(to: #selector(iCloudDelegate.iCloudFileUpdateDidEnd)) ?? false {
+            DispatchQueue.main.async { self.delegate?.iCloudFileUpdateDidEnd?() }
+        }
+
+        if self.verboseLogging { NSLog("[iCloud] Finished file update with NSMetadataQuery") }
+    }
+    
+}
